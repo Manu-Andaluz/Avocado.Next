bc1e0575c35775676315a1edba96e63e08a9d950
Manu-Andaluz <manupeandaluz@gmail.com>
2023-07-25 21:08:25 -0300

    adding port railway

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,5 +7,5 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_DEPLOY,
 });
